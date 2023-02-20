da7c43560bb9a153ab1cd8c11cc6498dd92e3fba
DragonFlameStrike <d.pankov@g.nsu.ru>
2023-02-20 20:59:37 +0700

    init commit

---
+++ src/main/java/com/big_case_club/depersonalization/DepersonalizationApplication.java
@@ -0,0 +1,13 @@
+package com.big_case_club.depersonalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepersonalizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepersonalizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/big_case_club/depersonalization/DepersonalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.big_case_club.depersonalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepersonalizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
