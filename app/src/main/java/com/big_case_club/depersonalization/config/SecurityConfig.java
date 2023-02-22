1df32601277060895e1c53a9441c24353e2f2c09
Misha <misha.solodkin0126@gmail.com>
2023-02-22 20:31:26 +0700

    Запретил доступ к /api/database не админам, добавил админа.

--- app/src/main/java/com/big_case_club/depersonalization/config/SecurityConfig.java
+++ app/src/main/java/com/big_case_club/depersonalization/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,8 +16,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.httpBasic();
+        http.authorizeHttpRequests().requestMatchers("/api/database/**").hasRole("ADMIN").anyRequest().permitAll().and().formLogin();
 
         return http.build();
+    }
+
+    @Bean
+
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
     }
 }
