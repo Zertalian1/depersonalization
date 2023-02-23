29a1f62e92b7be0687303cee02c9f86739bf471c
DragonFlameStrike <d.pankov@g.nsu.ru>
2023-02-23 12:11:15 +0700

    Добавил нерабочие тесты, нужно решить вопрос с Transactional

--- app/src/main/java/com/big_case_club/depersonalization/config/DatabaseDepersonalizeConfig.java
+++ app/src/main/java/com/big_case_club/depersonalization/config/DatabaseDepersonalizeConfig.java
@@ -1,7 +1,6 @@
 package com.big_case_club.depersonalization.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
