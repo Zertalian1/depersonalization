0d37dea984098ab45f9800a0ab450530d66e4673
Misha <misha.solodkin0126@gmail.com>
2023-02-26 15:21:04 +0700

    Включил конфигурацию CORS и испрваил путь авторизации

--- app/src/main/java/com/big_case_club/depersonalization/config/SecurityConfig.java
+++ app/src/main/java/com/big_case_club/depersonalization/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.big_case_club.depersonalization.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -9,6 +10,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -16,8 +23,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().formLogin().permitAll().and()
-                .authorizeHttpRequests().requestMatchers("/api/database/**").hasRole("ADMIN").anyRequest().permitAll();
+        http.csrf().disable().cors(Customizer.withDefaults()).formLogin().permitAll().and()
+                .authorizeHttpRequests().requestMatchers("/api/database/personalize/**").hasRole("ADMIN").anyRequest().permitAll();
 
         return http.build();
     }
@@ -30,5 +37,14 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(admin);
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
