37f2c7f5687874a25336f5873ecd96488de1c697
zertalian <Zertalian@mail.ru>
2023-02-27 17:33:50 +0700

    elfkbk vecjh

--- docker_postgres_init.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER connection2 WITH PASSWORD 'secret' CREATEDB;
-CREATE DATABASE BigCaseClubDepersonalize
-    WITH
-    OWNER = connection2
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
