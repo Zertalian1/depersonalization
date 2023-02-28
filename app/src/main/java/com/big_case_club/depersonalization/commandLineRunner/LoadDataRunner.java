e661ee0ad2f22f183f36d2a01c1263b651b4d5e8
DragonFlameStrike <d.pankov@g.nsu.ru>
2023-02-28 12:33:49 +0700

    Исправил автогенерацию

--- app/src/main/java/com/big_case_club/depersonalization/commandLineRunner/LoadDataRunner.java
+++ app/src/main/java/com/big_case_club/depersonalization/commandLineRunner/LoadDataRunner.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Component
 @AllArgsConstructor
@@ -19,31 +20,33 @@ public class LoadDataRunner {
     @Bean
     public CommandLineRunner loadData() {
         return (args) -> {
-            personalizeDataService.deleteAllData();
-            personalizeDataService.saveData(new PersonalizeData(
-                    0L,
-                    "John Smith",
-                    LocalDate.of(1980, 1, 1),
-                    "New York",
-                    "м",
-                    "123456789012",
-                    "123-456-789 00",
-                    "john.smith@gmail.com",
-                    "123 Main St.",
-                    "Passport",
-                    "123456789"));
-            personalizeDataService.saveData(new PersonalizeData(
-                    0L,
-                    "Петров Петр Петрович",
-                    LocalDate.of(1980, 4, 26),
-                    "Санкт-Петербург",
-                    "м",
-                    "123456789012",
-                    "123-456-789 00",
-                    "petrov@example.com",
-                    "Санкт-Петербург, ул. Лермонтова, д. 2",
-                    "паспорт",
-                    "123456789"));
+            List<PersonalizeData> data = personalizeDataService.viewDatabase("");
+            if(data.isEmpty()) {
+                personalizeDataService.saveData(new PersonalizeData(
+                        0L,
+                        "Иванов Иван Иваныч",
+                        LocalDate.of(2002, 3, 12),
+                        "Новосибирск",
+                        "м",
+                        "123666789012",
+                        "123-666-666 00",
+                        "d.pankov@g.nsu.ru",
+                        "Советская 19",
+                        "пасспорт",
+                        "123333789"));
+                personalizeDataService.saveData(new PersonalizeData(
+                        0L,
+                        "Петров Петр Петрович",
+                        LocalDate.of(1980, 4, 26),
+                        "Санкт-Петербург",
+                        "м",
+                        "123456789012",
+                        "123-456-789 00",
+                        "petrov@example.com",
+                        "Санкт-Петербург, ул. Лермонтова, д. 2",
+                        "паспорт",
+                        "123456789"));
+            }
         };
     }
 }
